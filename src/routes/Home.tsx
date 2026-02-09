9b9a89eba678d8c52726deb54052accf009b6865
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-02-09 22:43:32 +0100

    Refactor Home component to group assignments by language and label
    - Implemented logic to group assignments by language and label for better organization.
    - Replaced the previous list structure with nested ordered lists to display grouped assignments.
    - Utilized Solid.js's `<For>` component for rendering the grouped assignments dynamically.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,44 @@
 import { A } from '@solidjs/router'
+import { For } from 'solid-js'
 import { Assignment } from '../types.tsx'
 
 export default () => {
+	const groupedAssignments: Record<string, Record<string, Assignment[]>> = {}
+	Assignment.assignments.forEach((assignment) => {
+		if (!groupedAssignments[assignment.language]) {
+			groupedAssignments[assignment.language] = {
+				[assignment.label]: [assignment],
+			}
+		} else {
+			groupedAssignments[assignment.language][assignment.label] = [...(groupedAssignments[assignment.language][assignment.label] || []), assignment]
+		}
+	})
 	return (
 		<div style='width: fit-content; margin-left: auto; margin-right: auto;'>
 			<h1 style='text-align: center;'>Assignments</h1>
-			<ul style='padding-inline-start: 1ex;'>
-				{Assignment.assignments.map((assignment) => (
-					<li>
-						<A href={`/Assignment/${assignment.language.replaceAll(' / ', '-')}/${assignment.label}/${assignment.title}/${assignment.key}`}>{assignment.title}</A>
-					</li>
-				))}
-			</ul>
+			<For each={Object.entries(groupedAssignments)}>
+				{([language, labels]) => (
+					<ol>
+						<h2>{language}</h2>
+						<For each={Object.entries(labels)}>
+							{([label, assignments]) => (
+								<li>
+									<h3>{label}</h3>
+									<ol>
+										<For each={assignments}>
+											{(assignment) => (
+												<li>
+													<A href={`/Assignment/${assignment.language.replaceAll(' / ', '-')}/${assignment.label}/${assignment.title}/${assignment.key}`}>{assignment.title}</A>
+												</li>
+											)}
+										</For>
+									</ol>
+								</li>
+							)}
+						</For>
+					</ol>
+				)}
+			</For>
 		</div>
 	)
 }
