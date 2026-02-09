dd7cb40cbe9784a00ed52e9d86f0df576e6b3640
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-02-09 20:03:46 +0100

    Update Introduction assignment content with a detailed description and example code for adding two numbers

--- src/assignments/Introduction.tsx
+++ src/assignments/Introduction.tsx
@@ -1,7 +1,21 @@
 import { Assignment } from '../types.tsx'
 
+const content = [
+	`/**
+ * Hello World
+ *
+ * This is a simple introductory assignment to get you started with %THIS_SITE_NAME% and the JavaScript language.
+ * This assignment is easy. You just need to add two numbers (a and b) and assign the result to the 'answer' variable. Do it however you want.
+ */
+const a = 1
+const b = 2
+let answer = 0`,
+	'answer = ',
+	`a + b = answer`,
+]
+
 export default class extends Assignment {
 	constructor() {
-		super('b78b6e6e-5d21-41dd-91d2-1ab23dbb1625', 'Introduction', 'Introduction', 'JavaScript / TypeScript', [])
+		super('b78b6e6e-5d21-41dd-91d2-1ab23dbb1625', 'Introduction', 'Introduction', 'JavaScript / TypeScript', content)
 	}
 }
