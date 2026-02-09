7841a277c7d68ecfd7b380cfe4acf35ae785db78
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-02-09 19:25:36 +0100

    Update Home component to use Solid.js `<A>` component for navigation and improve styling
    - Replaced anchor tags with Solid.js's `<A>` component for assignment links.
    - Enhanced layout and styling for better visual presentation of assignments.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,14 @@
+import { A } from '@solidjs/router'
 import { Assignment } from '../types.tsx'
 
 export default () => {
 	return (
-		<div style='text-align: center'>
-			<h1>Home</h1>
-			<ul>
+		<div style='width: fit-content; margin-left: auto; margin-right: auto;'>
+			<h1 style='text-align: center;'>Assignments</h1>
+			<ul style='padding-inline-start: 1ex;'>
 				{Assignment.assignments.map((assignment) => (
 					<li>
-						<a href={`/Assignment/${assignment.language.replaceAll(' / ', '-')}/${assignment.label}/${assignment.title}/${assignment.key}`}>{assignment.title}</a>
+						<A href={`/Assignment/${assignment.language.replaceAll(' / ', '-')}/${assignment.label}/${assignment.title}/${assignment.key}`}>{assignment.title}</A>
 					</li>
 				))}
 			</ul>
