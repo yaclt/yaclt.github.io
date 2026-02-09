30bd512b2ea16054ae76155260d585830ebd77aa
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-02-09 09:17:10 +0100

    Add Playground route and component
    - Introduced a new `Playground` route in `index.tsx`.
    - Created `Playground.tsx` component to display a simple heading.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Route, Router, type RouteSectionProps } from '@solidjs/router'
 import Home from './routes/Home.tsx'
 import Assignment from './routes/Assignment.tsx'
 import './AssignmentBuilder.tsx'
+import Playground from './routes/Playground.tsx'
 
 const root = (props: RouteSectionProps<unknown>) => (
 	<>
@@ -20,6 +21,7 @@ render(() => (
 	<Router root={root}>
 		<Route path='' component={Home} />
 		<Route path='Assignment/*path' component={Assignment} />
+		<Route path='Playground' component={Playground} />
 		<Route path='*missingPage' component={Home} />
 	</Router>
 ), document.body!)
