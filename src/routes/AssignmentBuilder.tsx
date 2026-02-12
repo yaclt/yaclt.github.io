e2d89ba8db901336f4a63ab57ce6feea949d87b6
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-02-12 21:56:13 +0100

    Add dynamic segment handling in AssignmentBuilder component
    - Introduced functionality to dynamically manage multiple input segments for assignment codes.
    - Implemented a signal-based approach to update and display the JSON representation of the input values.
    - Enhanced user experience by allowing real-time updates as users modify the text areas.

--- src/routes/AssignmentBuilder.tsx
+++ src/routes/AssignmentBuilder.tsx
@@ -1,8 +1,46 @@
+import { createSignal, createUniqueId, For, type Signal } from 'solid-js'
+
 export default () => {
+	const codes: Signal<string>[] = []
+	const [segments, setSegments] = createSignal(1)
+	const [code, setCode] = createSignal('')
+
+	function updateCode(element: HTMLTextAreaElement) {
+		const value = JSON.stringify(element.value)
+		const index = parseInt(element.id.split('_').at(-1) ?? '0')
+		if (codes[index]) {
+			codes[index][1](value)
+		} else {
+			codes[index] = createSignal(value)
+		}
+
+		const codeList: string[] = []
+		codes.forEach((code) => {
+			codeList.push(code[0]())
+		})
+		setCode('[\n' + codeList.join(',\n') + '\n]')
+	}
+
 	return (
 		<div>
 			<h1>Assignment Builder</h1>
 			{crypto.randomUUID()}
+
+			<input id={createUniqueId()} type='number' min={1} value={segments()} onChange={(e) => setSegments(Number(e.target.value))} />
+
+			<div style='display: flex; flex-direction: column;'>
+				<For each={Array.from({ length: segments() })}>
+					{(_, index) => (
+						<textarea
+							id={'segment_' + index().toString()}
+							onInput={(e) => updateCode(e.target)}
+						/>
+					)}
+				</For>
+			</div>
+
+			<p>Code:</p>
+			<textarea value={code()} />
 		</div>
 	)
 }
